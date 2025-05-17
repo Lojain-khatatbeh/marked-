ee8d7ecf0e49102b86661f3a7ff9f3cc6f8bed28
Lojain-khatatbeh <lojainkatatbh@gmail.com>
2025-05-17 21:25:51 +0100

    Create سي.js

---
+++ .github/workflows/سي.js
@@ -0,0 +1,12 @@
+// دالة تحسب مجموع رقمين
+function sum(a, b) {
+  return a + b;
+}
+
+// استخدام الدالة
+const num1 = 5;
+const num2 = 7;
+const result = sum(num1, num2);
+
+console.log("المجموع هو:", result);
+
